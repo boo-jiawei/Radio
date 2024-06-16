136da07c56cf8aba34855cfc1fb3b92133778344
boo-jiawei <jiaweipung@gmail.com>
2024-06-16 13:18:39 +0800

    I didn't

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     fetch("https://de1.api.radio-browser.info/json/stations/topvote/20")
         .then((response) => response.json())
-    .then((data) => {
-        data
-        .forEach((station) => {
-            let track = document.createElement("div");
-            track.innerText = station.name;
-            track.addEventListener("click", () => {
-                audioElement.src = station.url_resolved;
-                audioElement.play();
-                currentTrackInfo.innerText = `Now playing: ${station.name}`;
-            });
-            trackListElement.appendChild(track);
-        });
-    .catch ((error) => alert("Error fetching stations:", error));
- });
+        .then((data) => {
+            data
+                .forEach((station) => {
+                    let track = document.createElement("div");
+                    track.innerText = station.name;
+                    track.addEventListener("click", () => {
+                        audioElement.src = station.url_resolved;
+                        audioElement.play();
+                        currentTrackInfo.innerText = `Now playing: ${station.name}`;
+                    });
+                    trackListElement.appendChild(track);
+                })
+        }).catch((error) => alert("Error fetching stations:", error));
+
 });
